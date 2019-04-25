47fc1f9826022ce5ea3499c2e4534398c51b72e8
webdevych <lee.smith870327@gmail.com>
2019-04-25 08:41:40 -0400

    Added abiity for users to like and unlike destinations.

--- server/models/destination.js
+++ server/models/destination.js
@@ -2,13 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DestinationSchema = new Schema({
-	title: { type: String },
+	title: { type: String, required: true },
 	dateCreated: { type: Date, default: Date.now },
-	description: { type: String },
+	description: { type: String, required: true },
 	user: {
 		type: Schema.Types.ObjectId,
 		ref: 'user',
+		required: true,
 	},
+	likedBy: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'user',
+		},
+	],
 });
+
+DestinationSchema.statics.toggleLike = function(destinationId, userId) {
+	const User = mongoose.model('user');
+
+	return this.findById(destinationId).then(destination => {
+		const array = destination.likedBy;
+		if (array.indexOf(userId) > -1) {
+			const index = array.indexOf(userId);
+			array.splice(index, 1);
+			return destination.save();
+		}
+		array.push(userId);
+		return destination.save();
+	});
+};
 
 mongoose.model('destination', DestinationSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	name: { type: String },
-	email: { type: String },
-	password: { type: String },
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
 });
 
 mongoose.model('user', UserSchema);
